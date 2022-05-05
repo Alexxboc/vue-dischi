436a8785e698153514d38348e94433356afedda5
Alexxboc <boccardi.alessandro@gmail.com>
2022-05-05 15:03:52 +0200

    First commit:
    -Create vue project
    -Import bootsrap, axios
    -Create sccs style sheets

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <header>
+      <div class="logo">
+        <img width="60" src="@/assets/logo-small.svg">
+      </div>
+      <!-- /.logo -->
+    </header>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style lang="scss">
+@import '@/assets/scss/style.scss';
+header {
+  background-color: $spotify-transparent;
+  padding: 1rem;
+}
+
+</style>
