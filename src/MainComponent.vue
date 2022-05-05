5ecff62b325c787458077d5d4d77d3d4b73570d2
Alexxboc <boccardi.alessandro@gmail.com>
2022-05-05 17:45:52 +0200

    add loader component

--- src/MainComponent.vue
+++ src/MainComponent.vue
@@ -1,43 +1,54 @@
 <template>
-     <main class="pt-6">
-      <div class="container">
-        <div class="row row-cols-5">
-          <div class="col gy-3" v-for="(album, index) in musicList" :key="index">
-              <CardComponent :poster="album.poster" :titolo="album.title" :autore="album.author" />
-          </div>
-          
-          <!-- /.col -->
+  <main class="pt-6">
+    <div class="container" v-if="!loading">
+      <div class="row row-cols-5">
+        <div class="col gy-3" v-for="(album, index) in musicList" :key="index">
+          <CardComponent
+            :poster="album.poster"
+            :titolo="album.title"
+            :autore="album.author"
+          />
         </div>
-        <!-- /.row row-cols-5 -->
+
+        <!-- /.col -->
       </div>
-      <!-- /.container -->
-    </main>
+      <!-- /.row row-cols-5 -->
+    </div>
+    <!-- /.container -->
+    <loaderComponent v-else />
+  </main>
 </template>
 
 <script>
 import axios from "axios";
-import CardComponent from "@/components/CardComponent.vue"
+import CardComponent from "@/components/CardComponent.vue";
+import loaderComponent from "@/components/LoaderComponent.vue"
 export default {
-  name: 'SiteMain',
+  name: "SiteMain",
   components: {
     CardComponent,
+    loaderComponent,
   },
   data() {
     return {
       link: "https://flynn.boolean.careers/exercises/api/array/music",
       response: null,
       musicList: null,
+      loading: true,
     };
   },
   mounted() {
-    axios.get(this.link).then((response) => {
-      console.log(response);
-      this.musicList = response.data.response;
-      // console.log(this.musicList);
-    })
-    .catch(error => {
-      console.log(error);
-    })
+    axios
+      .get(this.link)
+      .then((response) => {
+        console.log(response);
+        this.musicList = response.data.response;
+        // console.log(this.musicList);
+        this.loading = false;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   },
 };
 </script>
---
+++ src/components/LoaderComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <div class="loading d-flex justify-content-center align-items-center">
+      <h1 class="text-dark">Loading...</h1>
+    </div>
+    <!-- /.loading -->
+  </div>
+  <!-- /.container loading -->
+</template>
+
+<script>
+export default {
+  name: "loaderComponent",
+};
+</script>
+
+<style lang="scss" scoped>
+.loading {
+  height: 100vh;
+  width: 100vw;
+  background-color: white;
+}
+</style>
