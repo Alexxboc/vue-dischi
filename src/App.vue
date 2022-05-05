4ffafa3c0dc93496902e6eb0c0ca75a4f4147ebc
Alexxboc <boccardi.alessandro@gmail.com>
2022-05-05 16:21:35 +0200

    create header component

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,56 @@
 <template>
   <div id="app">
-    <header>
-      <div class="logo">
-        <img width="60" src="@/assets/logo-small.svg">
+    <SiteHeader />
+    <main class="pt-6">
+      <div class="container">
+        <div class="row row-cols-5">
+          <div class="col gy-3" v-for="(album, index) in musicList" :key="index">
+            <div class="card h-100 p-3 rounded-0 bg_transparent" style="width: 15rem">
+              <img :src="album.poster" class="card-img-top img-fluid" alt="" />
+              <div class="card-body text-dark text-center">
+                <h2 class="card-title text_white text-uppercase fs-4 fw-bold mt-3">{{album.title}}</h2>
+                <p class="card-text text_light fs-5 mt-4">{{album.author}}</p>
+              </div>
+            </div>
+          </div>
+          <!-- /.col -->
+        </div>
+        <!-- /.row row-cols-5 -->
       </div>
-      <!-- /.logo -->
-    </header>
+      <!-- /.container -->
+    </main>
   </div>
 </template>
 
 <script>
-
+import axios from "axios";
+import SiteHeader from "@/HeaderComponent.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    
-  }
-}
+    SiteHeader,
+  },
+  data() {
+    return {
+      link: "https://flynn.boolean.careers/exercises/api/array/music",
+      response: null,
+      musicList: null,
+    };
+  },
+  mounted() {
+    axios.get(this.link).then((response) => {
+      console.log(response);
+      this.musicList = response.data.response;
+      // console.log(this.musicList);
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  },
+};
 </script>
 
 <style lang="scss">
-@import '@/assets/scss/style.scss';
-header {
-  background-color: $spotify-transparent;
-  padding: 1rem;
-}
-
+@import "@/assets/scss/style.scss";
 </style>
