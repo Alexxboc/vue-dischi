563ec4fec1b909c9c41fa05b72cb895b84f772bc
Alexxboc <boccardi.alessandro@gmail.com>
2022-05-05 17:12:30 +0200

    Creating card component

--- src/MainComponent.vue
+++ src/MainComponent.vue
@@ -2,15 +2,8 @@
      <main class="pt-6">
       <div class="container">
         <div class="row row-cols-5">
-          <div class="col gy-3" v-for="(album, index) in musicList" :key="index">
-            <div class="card h-100 p-3 rounded-0 bg_transparent" style="width: 15rem">
-              <img :src="album.poster" class="card-img-top img-fluid" alt="" />
-              <div class="card-body text-dark text-center">
-                <h2 class="card-title text_white text-uppercase fs-4 fw-bold mt-3">{{album.title}}</h2>
-                <p class="card-text text_light fs-5 mt-4">{{album.author}}</p>
-              </div>
-            </div>
-          </div>
+          <div class="col gy-3" v-for="(album, index) in musicList" :key="index"></div>
+          <CardComponent :poster="album.poster" :titolo="album.title" :autore="album.author" />
           <!-- /.col -->
         </div>
         <!-- /.row row-cols-5 -->
@@ -21,10 +14,11 @@
 
 <script>
 import axios from "axios";
+import CardComponent from "@/components/CardComponent.vue"
 export default {
   name: 'SiteMain',
   components: {
-    
+    CardComponent,
   },
   data() {
     return {
