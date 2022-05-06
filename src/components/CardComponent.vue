e785c695138fab382f5e454a08dfcc1b1adbe786
Alexxboc <boccardi.alessandro@gmail.com>
2022-05-06 10:29:37 +0200

    add style

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -5,7 +5,7 @@
       <h2 class="card-title text_white text-uppercase fs-4 fw-bold mt-3">
         {{titolo}}
       </h2>
-      <p class="card-text text_light fs-5 mt-4 mb-0 fw-bolder">{{autore}}</p>
+      <p class="card-text text_light fs-5 mt-3 mb-0 fw-bolder">{{autore}}</p>
       <p class="card-text text_light fs-6 fw-bolder">{{anno}}</p>
     </div>
     <!-- /.card-body -->
